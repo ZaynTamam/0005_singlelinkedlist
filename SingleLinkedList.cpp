0b1c8db33478920721c3ab512ae6c978eddd9ddc
ZaynTamam <tamam.zain78@gmail.com>
2026-04-23 15:34:15 +0700

    Menambahkan metode addNode untuk menambahkan node baru ke LinkedList

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -19,4 +19,13 @@ class LinkedList
     {
         START = NULL;
     }
-    };
+
+    void addNode()
+    {
+        int nim;
+        cout << "/nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+    
